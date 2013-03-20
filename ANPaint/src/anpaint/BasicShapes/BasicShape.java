6045a14c815717221ebb831976adfeeeaef57ccb
Alex Craig <andeic@hotmail.com>
2013-03-19 21:50:41 -0400

    fixed up a few small things in the code

--- ANPaint/src/anpaint/BasicShapes/BasicShape.java
+++ ANPaint/src/anpaint/BasicShapes/BasicShape.java
@@ -1,25 +1,26 @@
-package anpaint.Graphics;
+package anpaint.BasicShapes;
 
 import java.awt.Color;
+import java.awt.Graphics;
 import java.awt.Point;
 import java.util.ArrayList;
 
 /*
-* The Graphic class defines the methods and members for the inherited classes,
+* The BasicShape class defines the methods and members for the inherited classes,
 * which are the various shapes that can be drawn.
 * This is part of the Composite Pattern
 */
-public abstract class Graphic {
+public abstract class BasicShape {
     protected Color _colour;
     protected Point[] _pointSet;
     protected boolean _style; //true solid, false dotted
     protected int _weight;
 
     //the method definitions that must be implemented in the sub classes
-    abstract void addGraphic(Graphic g);
-    abstract void removeGraphic(Graphic g);
-    abstract ArrayList<Graphic> getChildren();
-    abstract void draw();
+    abstract void add(BasicShape shape);
+    abstract void remove(BasicShape shape);
+    abstract ArrayList<BasicShape> getChildren();
+    abstract void paint(Graphics g);
     abstract void move(int dx, int dy);
     abstract void resize();
 }
